eafbe021806953061bbae28ebb35afcae789cbc7
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2026-05-16 03:38:50 +0600

    Fix case sensitivity in global trash directory path

--- dotmngr.ps1
+++ dotmngr.ps1
@@ -705,7 +705,7 @@ $globalTrashDirVal =$config.global.PSObject.Properties['trashDir']?.Value
 
 if ($globalTrash) {
   if ([string]::IsNullOrWhiteSpace([string]$globalTrashDirVal)) {
-    $globalTrashDir = Resolve-DotmngrPath -Path "%USERPROFILE%\trash\dotmngr"
+    $globalTrashDir = Resolve-DotmngrPath -Path "%USERPROFILE%\Trash\dotmngr"
   }
   else {
     $globalTrashDir = Resolve-DotmngrPath -Path ([string]$globalTrashDirVal)
